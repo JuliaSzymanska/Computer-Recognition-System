8d9cc486e1e5b64085e26f98238babc4a7bb1d47
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-03-23 12:19:28 +0100

    changed file structure

---
+++ Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/model/Article.java
@@ -0,0 +1,11 @@
+package tech.szymanskazdrzalik.ksr.ksr1.model;
+
+import org.jetbrains.annotations.Nullable;
+
+public class Article {
+    private String body;
+    private String title;
+    @Nullable
+    private String author;
+    private String dateline;
+}
