b1d2ac98cb9d68677eeab7aded1c0304466fac64
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-03-23 12:50:39 +0100

    working on creating article

--- Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/model/Article.java
+++ Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/model/Article.java
@@ -2,10 +2,49 @@ package tech.szymanskazdrzalik.ksr.ksr1.model;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Article {
     private String body;
     private String title;
     @Nullable
     private String author;
     private String dateline;
+
+    private FeatureVector featureVector;
+
+    private void createFeatureVector() {
+
+    }
+
+    public Article(String text) {
+        List<String> textSplitList = Arrays.asList(text.split(" "));
+        textSplitList.removeIf(StopList::contains);
+    }
+
+    private static class FeatureVector {
+        private int wordCount;
+        @Nullable
+        private String secondCurrency;
+        private int dayInYear;
+        private String location;
+        private String title;
+        @Nullable
+        private String author;
+        @Nullable
+        private String mostPopularCountry;
+        private String[] keyWords;
+        private int keyWordCount;
+        private int keyWordSaturation;
+        @Nullable
+        private String mostPopularKeyWord;
+        private int uniqueWordCount;
+
+        public FeatureVector(String text) {
+
+        }
+
+    }
 }
---
+++ Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/model/StopList.java
@@ -0,0 +1,37 @@
+package tech.szymanskazdrzalik.ksr.ksr1.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class StopList {
+    private static String[] words = new String[0];
+    private static void loadStopWords(String path) throws IOException {
+        loadStopWords(new File(path));
+    }
+
+    private static void loadStopWords(URL path) throws IOException {
+        loadStopWords(new File(path.getPath()));
+    }
+
+    private static void loadStopWords(File file) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fileInputStream.read(data);
+        fileInputStream.close();
+        String str = new String(data, StandardCharsets.UTF_8);
+        words = str.split(" ");
+    }
+
+    public static boolean contains(String word) {
+        return Arrays.asList(words).contains(word);
+    }
+
+}
