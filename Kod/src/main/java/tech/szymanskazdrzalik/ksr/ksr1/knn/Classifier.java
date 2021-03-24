495647314a024544b0da0cc2ed9d6081f118e9f1
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-03-24 17:44:53 +0100

    Implemented empty method in classifier

--- Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/knn/Classifier.java
+++ Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/knn/Classifier.java
@@ -1,4 +1,25 @@
 package tech.szymanskazdrzalik.ksr.ksr1.knn;
 
+import tech.szymanskazdrzalik.ksr.ksr1.metric.Metric;
+import tech.szymanskazdrzalik.ksr.ksr1.model.Article;
+import tech.szymanskazdrzalik.ksr.ksr1.model.Pair;
+
 public class Classifier {
+
+    private static String simulate(Metric metric, int k){
+        return "";
+    }
+
+    private static Article[] findKNearestNeighbours(Article article, int k){
+        return null;
+    }
+
+    private static Pair calculateDistance(Article article, Article[] trainingArticles){
+        return null;
+    }
+
+    private static String classify(Article article){
+        return "";
+    }
+
 }
