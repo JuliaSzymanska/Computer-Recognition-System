f4b693b2e500196a60ca7629c04af341e41b2536
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-03-26 22:05:35 +0100

    Addded chebyshec metric

--- Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/metric/ChebyshevMetric.java
+++ Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/metric/ChebyshevMetric.java
@@ -1,8 +1,15 @@
 package tech.szymanskazdrzalik.ksr.ksr1.metric;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ChebyshevMetric implements Metric {
     @Override
     public double calculateDistance(double[] article, double[] trainingArticle) {
-        return 0;
+        ArrayList<Double> listOfDifferences = new ArrayList<Double>();
+        for (int i = 0; i < article.length; i++) {
+            listOfDifferences.add(Math.abs(article[i] - trainingArticle[i]));
+        }
+        return Collections.max(listOfDifferences);
     }
 }
