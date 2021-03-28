4d7191f98abac254ca758a924d17f5644e82e057
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-03-29 00:34:19 +0200

    Added choice of cechy

--- Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/Main.java
+++ Kod/src/main/java/tech/szymanskazdrzalik/ksr/ksr1/Main.java
@@ -10,6 +10,7 @@ import tech.szymanskazdrzalik.ksr.ksr1.model.Article;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -42,7 +43,7 @@ public class Main {
                 in.next();
             }
         }
-        System.out.println("Podaj procent artykułów testowych: ");
+        System.out.println("Podaj procent artykułów treningowych: ");
         int trainings = 0;
         while (in.hasNext()) {
             if (in.hasNextInt()) {
@@ -54,6 +55,49 @@ public class Main {
                 System.out.println("Podaj procent artykułów treningowych: ");
                 in.next();
             }
+        }
+
+        System.out.println("Czy chcesz wybrać zestaw cech do klasyfikacji: \n1. Tak\n2. Nie");
+        int isSet = 0;
+        while (in.hasNext()) {
+            if (in.hasNextInt()) {
+                isSet = in.nextInt();
+                if (isSet == 1 || isSet == 2) {
+                    break;
+                }
+            } else {
+                System.out.println("Czy chcesz wybrać zestaw cech do klasyfikacji: \n1. Tak\n2. Nie");
+                in.next();
+            }
+        }
+        System.out.println("Cechy do wyboru: ");
+        System.out.println("1. Liczba słów");
+        System.out.println("2. Autor");
+        System.out.println("3. Liczba unikatowych słów");
+        System.out.println("4. Data");
+        System.out.println("5. Lokalizacja");
+        System.out.println("6. Tytuł");
+        System.out.println("7. Najczęściej występująca nazwa państwa");
+        System.out.println("8. Kluczowe słowa");
+        System.out.println("9. Liczba kluczowych słów");
+        System.out.println("10. Nasycenie tekstu słowani kluczowymi");
+        System.out.println("11. Najczęściej występujące słowo kluczowe");
+        String set = "";
+        while (in.hasNext()) {
+            set = in.nextLine();
+            if (set.trim().length() > 0) {
+                break;
+            }
+        }
+
+        boolean[] booleanSet = new boolean[]{false, false, false, false, false, false, false, false, false, false, false};
+        int[] stringSet = Arrays.stream(set.trim().replaceAll(" +", " ").split(" ")).mapToInt(Integer::parseInt).toArray();
+        try{
+        for (int i : stringSet) {
+            booleanSet[i - 1] = true;
+        }} catch (Exception e){
+            System.out.println("Z powodu bledu, wybrane zostaly wszystkie cechy. ");
+            Arrays.fill(booleanSet, true);
         }
 
         Metric metricForClass;
@@ -79,11 +123,10 @@ public class Main {
         }
         double properlyClassified = 0;
         System.out.println("\nRozpoczęto klasyfikację.\n");
-        Classifier classifier = new Classifier(trainingSet, metricForClass, k, new boolean[]{true, true, true, true, true, true, true, true, true, true, true});
+        Classifier classifier = new Classifier(trainingSet, metricForClass, k, booleanSet);
         for (Article a : testSet) {
             String place = classifier.simulate(a);
             String place2 = a.getPlaces()[0];
-            System.out.println(place2 + "       " + place);
             if (place2.equals(place)) {
                 properlyClassified += 1;
             }
