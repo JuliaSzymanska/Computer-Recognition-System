698db2a562f525fb0e5d311090563dee2b325d6e
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-04-18 21:41:42 +0200

    Rotated plots

--- plot.py
+++ plot.py
@@ -30,30 +30,30 @@ import pandas as pd
 # plt.show()
 
 
-# # cechy
-# data = [[1, 0.60, 0.43, 0.60, 0.50],
-#         [2, 0.94, 0.95, 0.94, 0.95],
-#         [3, 0.95, 0.95, 0.95, 0.95],
-#         [4, 0.72, 0.72, 0.72, 0.72],
-#         [5, 0.95, 0.96, 0.95, 0.96],
-#         [6, 0.60, 0.52, 0.60, 0.56],
-#         ]
-#
-#
-# df = pd.DataFrame(data, columns=["Cechy", "Accuracy", "Precision", "Recall", "F1"])
-# df.plot(x="Cechy", y=["Accuracy", "Precision", "Recall", "F1"], kind="bar", figsize=(8, 8))
-# plt.show()
-
-
-# proporcje
-data = [[10, 0.59, 0.43, 0.71, 0.64],
-        [25, 0.85, 0.95, 0.81, 0.83],
-        [50, 0.91, 0.95, 0.90, 0.91],
-        [70, 0.93, 0.72, 0.93, 0.93],
-        [95, 0.98, 0.96, 0.97, 0.98],
+# cechy
+data = [[1, 0.60, 0.43, 0.60, 0.50],
+        [2, 0.94, 0.95, 0.94, 0.95],
+        [3, 0.95, 0.95, 0.95, 0.95],
+        [4, 0.72, 0.72, 0.72, 0.72],
+        [5, 0.95, 0.96, 0.95, 0.96],
+        [6, 0.60, 0.52, 0.60, 0.56],
         ]
 
 
-df = pd.DataFrame(data, columns=["Proporcje", "Accuracy", "Precision", "Recall", "F1"])
-df.plot(x="Proporcje", y=["Accuracy", "Precision", "Recall", "F1"], kind="bar", figsize=(8, 8))
+df = pd.DataFrame(data, columns=["Cechy", "Accuracy", "Precision", "Recall", "F1"])
+df.plot(x="Cechy", y=["Accuracy", "Precision", "Recall", "F1"], kind="bar", figsize=(8, 8), rot=0)
 plt.show()
+
+
+# # proporcje
+# data = [[10, 0.59, 0.43, 0.71, 0.64],
+#         [25, 0.85, 0.95, 0.81, 0.83],
+#         [50, 0.91, 0.95, 0.90, 0.91],
+#         [70, 0.93, 0.72, 0.93, 0.93],
+#         [95, 0.98, 0.96, 0.97, 0.98],
+#         ]
+#
+#
+# df = pd.DataFrame(data, columns=["Proporcje", "Accuracy", "Precision", "Recall", "F1"])
+# df.plot(x="Proporcje", y=["Accuracy", "Precision", "Recall", "F1"], kind="bar", figsize=(8, 8), rot=0)
+# plt.show()
