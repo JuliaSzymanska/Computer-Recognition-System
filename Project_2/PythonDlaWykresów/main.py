a7ff5ee10c1cb3b6ea0a369b79c3476d524ce08f
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-09 14:40:33 +0200

    wykres

--- Project_2/PythonDlaWykresów/main.py
+++ Project_2/PythonDlaWykresów/main.py
@@ -19,9 +19,23 @@ def draw_severity():
     plt.tight_layout()
     plt.show()
 
+def draw_duration():
+    plt.xticks(range(0, 24))
+    plt.grid(linestyle='dotted')
+    plt.plot([0, 1.5, 3], [1, 1, 0], label="Krótki Czas Trwania")
+    plt.plot([1.5, 3, 5.5, 7], [0, 1, 1, 0], label="Średni Czas Trwania")
+    plt.plot([5.5, 7, 12], [0, 1, 1], label="Długi Czas Trwania")
+    plt.xlabel('Czas Trwania W Godzinach')
+    plt.ylabel('Stopień Przynależności')
+    plt.title("Funkcja Przynależności Zbioru Rozmytego Czasu Trwania Wypadku")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    draw_severity()
+    draw_duration()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
