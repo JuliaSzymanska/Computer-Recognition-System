365238432586a8cb2a4ecc097b5a90d08fbb985a
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-10 09:35:15 +0200

    f

--- Project_2/PythonDlaWykresów/main.py
+++ Project_2/PythonDlaWykresów/main.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 def draw_severity():
     plt.xticks([1, 2, 3, 4])
     plt.grid(linestyle='dotted')
+    plt.plot([1, 2, 3, 4], [1, 2.0 / 3, 1.0 / 3, 0], '--')
+    plt.plot([1, 2, 3, 4], [0, 1.0 / 3, 2.0 / 3, 1], '--')
     plt.scatter([1, 2, 3, 4], [1, 2.0 / 3, 1.0 / 3, 0], label="Mały Wpływ")
     plt.scatter([1, 2, 3, 4], [0, 1.0 / 3, 2.0 / 3, 1], label="Duży Wpływ")
     plt.xlabel('Dotkliwość')
@@ -134,6 +136,6 @@ def draw_principation():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    draw_principation()
+    draw_severity()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
