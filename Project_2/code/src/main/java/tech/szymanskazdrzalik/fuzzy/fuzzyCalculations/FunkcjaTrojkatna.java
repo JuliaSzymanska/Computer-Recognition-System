cad6d919af6dc3d6d70a488e1dcc12e5bb5a638b
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-22 13:48:46 +0200

    rename

---
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/fuzzyCalculations/FunkcjaTrojkatna.java
@@ -0,0 +1,7 @@
+package tech.szymanskazdrzalik.fuzzy.fuzzyCalculations;
+
+public class FunkcjaTrojkatna extends FunkcjaTrapezoidalna{
+    public FunkcjaTrojkatna(double start, double highestPoint, double end) {
+        super(start, highestPoint, highestPoint, end);
+    }
+}
--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/fuzzyCalculations/funkcjaTrojkatna.java
+++
@@ -1,7 +0,0 @@
-package tech.szymanskazdrzalik.fuzzy.fuzzyCalculations;
-
-public class funkcjaTrojkatna extends FunkcjaTrapezoidalna{
-    public funkcjaTrojkatna(double start, double highestPoint, double end) {
-        super(start, highestPoint, highestPoint, end);
-    }
-}
