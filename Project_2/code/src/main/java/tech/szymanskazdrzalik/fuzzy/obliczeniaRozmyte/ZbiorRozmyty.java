1666eff61da33574936fa6de62b86fb40b39a248
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-23 18:59:40 +0200

    zmiana

--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/ZbiorRozmyty.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/ZbiorRozmyty.java
@@ -1,17 +1,48 @@
 package tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 public interface ZbiorRozmyty extends FunkcjaPrzynaleznosci  {
-    Double getWysokosc();
-    <T> List<T> getNosnik(Map<T, Double> objectDoubleMap);
-    <T> List<T> getPrzekrojAlfa(Map<T, Double> objectDoubleMap, Double alfa);
-    <T> Double liczbaKardynalna(Map<T, Double> objectDoubleMap);
-    Double iloczynZbiorow(ZbiorRozmyty zbiorRozmyty, Double x);
-    Double sumaZbiorow(ZbiorRozmyty zbiorRozmyty, Double x);
-    Double dopelnienieZbioru(Double x);
+    default Double getWysokosc() {
+        return 1.0;
+    }
+    default <T> List<T> getNosnik(Map<T, Double> objectDoubleMap) {
+        return this.getPrzekrojAlfa(objectDoubleMap, 0.0);
+    }
 
+    default <T> List<T> getPrzekrojAlfa(Map<T, Double> objectDoubleMap, Double alfa) {
+        List<T> list = new ArrayList<>();
+        objectDoubleMap.forEach(new BiConsumer<>() {
+            @Override
+            public void accept(T o, Double aDouble) {
+                if (ZbiorRozmyty.this.przynaleznosc(aDouble) > alfa) {
+                    list.add(o);
+                }
+            }
 
+            @Override
+            public BiConsumer<T, Double> andThen(BiConsumer<? super T, ? super Double> after) {
+                return BiConsumer.super.andThen(after);
+            }
+        });
+        return list;
+    }
+    default <T> Double liczbaKardynalna(Map<T, Double> objectDoubleMap) {
+        final Double[] sum = {0.0};
+        objectDoubleMap.forEach((t, aDouble) -> sum[0] += ZbiorRozmyty.this.przynaleznosc(aDouble));
+        return sum[0];
+    }
+    default Double iloczynZbiorow(ZbiorRozmyty zbiorRozmyty, Double x) {
+        return Math.min(this.przynaleznosc(x), zbiorRozmyty.przynaleznosc(x));
+    }
 
+    default Double sumaZbiorow(ZbiorRozmyty zbiorRozmyty, Double x) {
+        return Math.max(this.przynaleznosc(x), zbiorRozmyty.przynaleznosc(x));
+    }
+    default Double dopelnienieZbioru(Double x) {
+        return 1 - this.przynaleznosc(x);
+    }
 }
