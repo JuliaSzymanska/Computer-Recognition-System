4cae8800e4abcacf4e0cb8fb6d332e48995f3a54
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-05-28 17:43:54 +0200

    Added javadoc

--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/AbstractZbiorRozmyty.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/AbstractZbiorRozmyty.java
@@ -2,20 +2,20 @@ package tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte;
 
 public abstract class AbstractZbiorRozmyty implements ZbiorRozmyty {
     private final Double poczatekPrzestrzeniRozwarzan;
-    private final Double koniecPrzestrzeniRozwarzan;
+    private final Double koniecPrzestrzeniRozwazan;
 
     public AbstractZbiorRozmyty(Double poczatekPrzestrzeniRozwarzan, Double koniecPrzestrzeniRozwarzan) {
         this.poczatekPrzestrzeniRozwarzan = poczatekPrzestrzeniRozwarzan;
-        this.koniecPrzestrzeniRozwarzan = koniecPrzestrzeniRozwarzan;
+        this.koniecPrzestrzeniRozwazan = koniecPrzestrzeniRozwarzan;
     }
 
-    public Double getPoczatekPrzestrzeniRozwarzan() {
+    public Double getPoczatekPrzestrzeniRozwazan() {
         return this.poczatekPrzestrzeniRozwarzan;
     }
 
-    public Double getKoniecPrzestrzeniRozwarzan() {
+    public Double getKoniecPrzestrzeniRozwazan() {
 
-        return this.koniecPrzestrzeniRozwarzan;
+        return this.koniecPrzestrzeniRozwazan;
     }
 
 }
