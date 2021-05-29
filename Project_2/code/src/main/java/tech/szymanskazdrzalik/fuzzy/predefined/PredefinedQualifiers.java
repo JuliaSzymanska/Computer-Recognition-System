cff404a430615f446cf4fbd5ecfd772f492c2316
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-29 19:16:34 +0200

    fix qualifiers

--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/predefined/PredefinedQualifiers.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/predefined/PredefinedQualifiers.java
@@ -7,6 +7,7 @@ import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.FunkcjaTrapezoidalna;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.Cisnienie;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.IloscOpadow;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.PredkoscWiatru;
+import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.Temperatura;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.Widocznosc;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.Wilgotnosc;
 
@@ -22,12 +23,13 @@ public class PredefinedQualifiers {
     }
 
     private static void init() {
-        kwalifikatorList.add(new Etykieta<>("były przy słabym wietrze", new FunkcjaTrapezoidalna<>(0.0, 0.0, 3.0, 3.5, 0.0, 40.0, new PredkoscWiatru())));
-        kwalifikatorList.add(new Etykieta<>("były przy wichrze", new FunkcjaTrapezoidalna<>(17.0, 20.0, 27.0, 30.0, 0.0, 40.0, new PredkoscWiatru())));
-        kwalifikatorList.add(new Etykieta<>("były przy niewielkich opadach", new FunkcjaTrapezoidalna<>(0.0, 0.0, 0.1, 0.2, 0.0, 0.5, new IloscOpadow())));
-        kwalifikatorList.add(new Etykieta<>("były przy pełnej widoczności", new FunkcjaTrapezoidalna<>(2.0, 3.0, 5.0, 10.0, 0.0, 10.0, new Widocznosc())));
-        kwalifikatorList.add(new Etykieta<>("były przy umiarkowanym ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 28.75, 27.0, 32.0, new Cisnienie())));
-        kwalifikatorList.add(new Etykieta<>("były przy bardzo suchym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 4.0, 27.0, 32.0, new Wilgotnosc())));
+        kwalifikatorList.add(new Etykieta<>("będących przy słabym wietrze", new FunkcjaTrapezoidalna<>(0.0, 0.0, 3.0, 3.5, 0.0, 40.0, new PredkoscWiatru())));
+        kwalifikatorList.add(new Etykieta<>("będących przy wichrze", new FunkcjaTrapezoidalna<>(17.0, 20.0, 27.0, 30.0, 0.0, 40.0, new PredkoscWiatru())));
+        kwalifikatorList.add(new Etykieta<>("będących przy niewielkich opadach", new FunkcjaTrapezoidalna<>(0.0, 0.0, 0.1, 0.2, 0.0, 0.5, new IloscOpadow())));
+        kwalifikatorList.add(new Etykieta<>("będących przy pełnej widoczności", new FunkcjaTrapezoidalna<>(2.0, 3.0, 5.0, 10.0, 0.0, 10.0, new Widocznosc())));
+        kwalifikatorList.add(new Etykieta<>("będących przy umiarkowanym ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 28.75, 27.0, 32.0, new Cisnienie())));
+        kwalifikatorList.add(new Etykieta<>("będących przy bardzo suchym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 4.0, 27.0, 32.0, new Wilgotnosc())));
+        kwalifikatorList.add(new Etykieta<>("będących przy ciepłej temperaturze", new FunkcjaTrapezoidalna<>(63.0, 71.0, 80.0, 90.0, -16.0, 104.0, new Temperatura())));
 
     }
 
