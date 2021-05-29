50ce05311780d3998820eca575e5177c40b6e778
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-29 18:23:45 +0200

    yeee

--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/predefined/PredefinedQuantifiers.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/predefined/PredefinedQuantifiers.java
@@ -2,21 +2,45 @@ package tech.szymanskazdrzalik.fuzzy.predefined;
 
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Etykieta;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.FunkcjaGausowska;
+import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.FunkcjaTrojkatna;
 import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Kwantyfikator;
-import tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte.Zmienne.PobierzWartosc;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PredefinedQuantifiers {
-    private static List<Kwantyfikator> kwantyfikatorList = new ArrayList<>();
+    private final static double koniecUniversum = 300;
+    private static final List<Kwantyfikator> kwantyfikatorList = new ArrayList<>();
 
-    private PredefinedQuantifiers() {}
+    private PredefinedQuantifiers() {
+        init();
+    }
 
-    private void init() {
+    private static PredefinedQuantifiers INSTANCE = new PredefinedQuantifiers();
+
+    private static void init() {
         kwantyfikatorList.add(new Kwantyfikator(
                 new Etykieta<>("Niewiele", new FunkcjaGausowska<>(0.1, 1.0, 0.0, 0.0, 1.0, aDouble -> aDouble)),
                 false));
+        kwantyfikatorList.add(new Kwantyfikator(
+                new Etykieta<>("Około 1/4", new FunkcjaGausowska<>(0.1, 1.0, 0.25, 0.0, 1.0, aDouble -> aDouble)),
+                false));
+        kwantyfikatorList.add(new Kwantyfikator(
+                new Etykieta<>("Około połowy", new FunkcjaGausowska<>(0.1, 1.0, 0.5, 0.0, 1.0, aDouble -> aDouble)),
+                false));
+        kwantyfikatorList.add(new Kwantyfikator(
+                new Etykieta<>("Większość", new FunkcjaGausowska<>(0.1, 1.0, 0.75, 0.0, 1.0, aDouble -> aDouble)),
+                false));
+        kwantyfikatorList.add(new Kwantyfikator(
+                new Etykieta<>("Prawie Wszystkie", new FunkcjaGausowska<>(0.1, 1.0, 1.0, 0.0, 1.0, aDouble -> aDouble)),
+                false));
+        kwantyfikatorList.add(new Kwantyfikator(new Etykieta<>("Ponizej 10", new FunkcjaTrojkatna<Double>(0.0, 0.0, 10.0, 0.0, koniecUniversum, aDouble -> aDouble)), true));
+        kwantyfikatorList.add(new Kwantyfikator(new Etykieta<>("Ponizej 10", new FunkcjaTrojkatna<Double>(0.0, 0.0, 10.0, 0.0, koniecUniversum, aDouble -> aDouble)), true));
+        kwantyfikatorList.add(new Kwantyfikator(new Etykieta<>("Ponizej 10", new FunkcjaTrojkatna<Double>(0.0, 0.0, 10.0, 0.0, koniecUniversum, aDouble -> aDouble)), true));
+        kwantyfikatorList.add(new Kwantyfikator(new Etykieta<>("Ponizej 10", new FunkcjaTrojkatna<Double>(0.0, 0.0, 10.0, 0.0, koniecUniversum, aDouble -> aDouble)), true));
+        kwantyfikatorList.add(new Kwantyfikator(new Etykieta<>("Ponizej 10", new FunkcjaTrojkatna<Double>(0.0, 0.0, 10.0, 0.0, koniecUniversum, aDouble -> aDouble)), true));
+        kwantyfikatorList.add(new Kwantyfikator(new Etykieta<>("Ponizej 10", new FunkcjaTrojkatna<Double>(0.0, 0.0, 10.0, 0.0, koniecUniversum, aDouble -> aDouble)), true));
+
     }
 
     public List<Kwantyfikator> getKwantyfikatorList() {
