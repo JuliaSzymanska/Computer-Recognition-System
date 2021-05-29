31fe4d98f56e810bdef6755c97c1706137e3298d
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-05-29 14:54:34 +0200

    miary jakosci

--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/MiaryJakosci.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/MiaryJakosci.java
@@ -2,27 +2,6 @@ package tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte;
 
 public class MiaryJakosci {
 
-    public static class BrakKwalifikatora extends Exception{
-        public BrakKwalifikatora() {
-        }
-
-        public BrakKwalifikatora(String message) {
-            super(message);
-        }
-
-        public BrakKwalifikatora(String message, Throwable cause) {
-            super(message, cause);
-        }
-
-        public BrakKwalifikatora(Throwable cause) {
-            super(cause);
-        }
-
-        public BrakKwalifikatora(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-            super(message, cause, enableSuppression, writableStackTrace);
-        }
-    }
-
     /**
      * T1
      * To jest po prostu wartość funkcji przynależności obiektów dla kwalifikatora and z funckją przynależności zrobioną z andów funkcji przynależności tego sumaryzatora podzielonej przez funkcję przynależności obiektów dla kwalifikatora
@@ -78,19 +57,21 @@ public class MiaryJakosci {
     /**
      * T3
      */
+    // TODO: 29.05.2021
     public static Double stopienPokrycia(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
         var x = Utils.iloczyn(podsumowanieLingwistyczne.getSumaryzator());
-        if(podsumowanieLingwistyczne.getKwalifikator() == null){
-            return ((double)x.getNosnik(podsumowanieLingwistyczne.getPodmioty()).size()) / podsumowanieLingwistyczne.getPodmioty().size();
+        if (podsumowanieLingwistyczne.getKwalifikator() == null) {
+            return ((double) x.getNosnik(podsumowanieLingwistyczne.getPodmioty()).size()) / podsumowanieLingwistyczne.getPodmioty().size();
         }
         x = x.iloczynZbiorow(podsumowanieLingwistyczne.getKwalifikator().getAbstractZbiorRozmyty());
-        return ((double)x.getNosnik(podsumowanieLingwistyczne.getPodmioty()).size()) /
+        return ((double) x.getNosnik(podsumowanieLingwistyczne.getPodmioty()).size()) /
                 podsumowanieLingwistyczne.getKwalifikator().getAbstractZbiorRozmyty().getNosnik(podsumowanieLingwistyczne.getPodmioty()).size();
     }
 
     /**
      * T4
      */
+    // TODO: 29.05.2021
     public static Double stopienTrafnosci(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
         var x = stopienPokrycia(podsumowanieLingwistyczne);
         var iloczyn = 1;
@@ -102,7 +83,101 @@ public class MiaryJakosci {
     }
 
 
-public static Double dlugoscPodsumowania(PodsumowanieLingwistyczne podsumowanieLingwistyczne){}
+    /**
+     * T5
+     */
 
+    public static Double dlugoscPodsumowania(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        return 2 * Math.pow(0.5, podsumowanieLingwistyczne.getPodmioty().size());
+    }
+
+    /**
+     * T6
+     */
+    // TODO: 29.05.2021
+    public static Double stopienNieprecyzyjnosciKwantyfikatora(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        var z = Utils.iloczyn(podsumowanieLingwistyczne.getSumaryzator());
+        int count = 0;
+        for (var x : podsumowanieLingwistyczne.getPodmioty()) {
+            if (podsumowanieLingwistyczne.getKwantyfikator().getEtykieta().getAbstractZbiorRozmyty().przynaleznosc(z.przynaleznosc(x)) > 0) {
+                count++;
+            }
+        }
+        return (double) count / podsumowanieLingwistyczne.getPodmioty().size();
+    }
+
+    /**
+     * T7
+     */
+    // TODO: 29.05.2021
+    public static Double stopienKardynalnosciWzglednejKwantyfiaktora(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        var z = Utils.iloczyn(podsumowanieLingwistyczne.getSumaryzator());
+        double suma = 0;
+        for (var x : podsumowanieLingwistyczne.getPodmioty()) {
+            suma += podsumowanieLingwistyczne.getKwantyfikator().getEtykieta().getAbstractZbiorRozmyty().przynaleznosc(z.przynaleznosc(x));
+        }
+        suma /= podsumowanieLingwistyczne.getPodmioty().size();
+//        if (podsumowanieLingwistyczne.getKwantyfikator().getJestAbsolutny()) {
+//            return 1 - (suma) / podsumowanieLingwistyczne.getPodmioty().size())
+//        }
+        return 1 - suma;
+    }
+
+    /**
+     * T8
+     */
+    public static Double stopienKardynalnosciWzglednejSumaryzatora(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        double stopienPierwiastka = 1.0 / podsumowanieLingwistyczne.getSumaryzator().size();
+        double iloczyn = 1;
+        for (var x : podsumowanieLingwistyczne.getSumaryzator()) {
+            iloczyn *= x.getAbstractZbiorRozmyty().liczbaKardynalna(podsumowanieLingwistyczne.getPodmioty());
+            iloczyn /= podsumowanieLingwistyczne.getPodmioty().size();
+        }
+        return 1.0 - Math.pow(iloczyn, stopienPierwiastka);
+    }
+
+    /**
+     * T9
+     */
+    public static Double stopienNieprecyzyjnosciKwalifikatora(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        return 1.0 - podsumowanieLingwistyczne.getKwalifikator().getAbstractZbiorRozmyty().stopienRozmycia(podsumowanieLingwistyczne.getPodmioty());
+    }
+
+    /**
+     * T10
+     */
+    public static Double stopienKardynalnosciWzglednejKwalifikatora(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        double iloczyn = podsumowanieLingwistyczne.getKwalifikator().getAbstractZbiorRozmyty().liczbaKardynalna(podsumowanieLingwistyczne.getPodmioty());
+        iloczyn /= podsumowanieLingwistyczne.getPodmioty().size();
+        return 1.0 - iloczyn;
+    }
+
+    /**
+     * T11
+     */
+    public static Double dlugoscKwalifikatora(PodsumowanieLingwistyczne podsumowanieLingwistyczne) {
+        return 1.0;
+    }
+
+    public static class BrakKwalifikatora extends Exception {
+        public BrakKwalifikatora() {
+        }
+
+        public BrakKwalifikatora(String message) {
+            super(message);
+        }
+
+        public BrakKwalifikatora(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public BrakKwalifikatora(Throwable cause) {
+            super(cause);
+        }
+
+        public BrakKwalifikatora(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+            super(message, cause, enableSuppression, writableStackTrace);
+        }
+    }
 
 }
