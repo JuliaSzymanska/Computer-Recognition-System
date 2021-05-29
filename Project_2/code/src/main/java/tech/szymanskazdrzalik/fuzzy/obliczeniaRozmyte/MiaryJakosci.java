fd67a7126e4280e1078167298def23484890393a
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-05-29 11:48:52 +0200

    Dodanie miar jakosci pustej kalsy

---
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/MiaryJakosci.java
@@ -0,0 +1,9 @@
+package tech.szymanskazdrzalik.fuzzy.obliczeniaRozmyte;
+
+public class MiaryJakosci {
+
+    public Double stopienPrawdziwosci(){
+
+    }
+
+}
--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/PodsumowanieLingwistyczne.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/obliczeniaRozmyte/PodsumowanieLingwistyczne.java
@@ -18,6 +18,4 @@ public class PodsumowanieLingwistyczne {
         this.kwalifikator = kwalifikator;
     }
 
-
-
 }
