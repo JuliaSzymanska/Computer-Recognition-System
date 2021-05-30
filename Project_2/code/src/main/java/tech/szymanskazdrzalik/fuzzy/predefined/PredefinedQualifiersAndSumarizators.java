968a9e44ca6bed261d52c10d0df2fbfcad993a86
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-05-30 16:08:59 +0200

    stale

--- Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/predefined/PredefinedQualifiersAndSumarizators.java
+++ Project_2/code/src/main/java/tech/szymanskazdrzalik/fuzzy/predefined/PredefinedQualifiersAndSumarizators.java
@@ -89,6 +89,30 @@ public class PredefinedQualifiersAndSumarizators {
         return odleglosc;
     }
 
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU = 0.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU = 40.0;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW = 0.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW = 0.5;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC= 0.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC = 10.0;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_CISNIENIE= 27.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_CISNIENIE = 32.0;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_WILGOTNOSC = 4.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_WILGOTNOSC = 100.0;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA = -16.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA = 104.0;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_ODLEGLOSC = 0.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_ODLEGLOSC = 6.0;
+
+    public static final Double POCZATEK_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA = 0.0;
+    public static final Double KONIEC_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA = 24.0;
+
     private static void init() {
         predkoscWiatru = new ZmiennaLingwistyczna(new ArrayList<>());
         iloscOpadow = new ZmiennaLingwistyczna(new ArrayList<>());
@@ -99,58 +123,58 @@ public class PredefinedQualifiersAndSumarizators {
         temperaturaOdczuwalna = new ZmiennaLingwistyczna(new ArrayList<>());
         czasTrwania = new ZmiennaLingwistyczna(new ArrayList<>());
         odleglosc = new ZmiennaLingwistyczna(new ArrayList<>());
-        predkoscWiatru.add(new Etykieta<>("przy słabym wietrze", new FunkcjaTrapezoidalna<>(0.0, 0.0, 3.0, 3.5, 0.0, 40.0, new PredkoscWiatru())));
-        predkoscWiatru.add(new Etykieta<>("przy umiarkowanym wietrze", new FunkcjaTrapezoidalna<>(3.0, 3.5, 8.0, 9.0, 0.0, 40.0, new PredkoscWiatru())));
-        predkoscWiatru.add(new Etykieta<>("przy silnym wietrze", new FunkcjaTrapezoidalna<>(8.0, 9.0, 17.0, 20.0, 0.0, 40.0, new PredkoscWiatru())));
-        predkoscWiatru.add(new Etykieta<>("przy wichrze", new FunkcjaTrapezoidalna<>(17.0, 20.0, 27.0, 30.0, 0.0, 40.0, new PredkoscWiatru())));
-        predkoscWiatru.add(new Etykieta<>("przy huraganie", new FunkcjaTrapezoidalna<>(27.0, 30.0, 40.0, 40.0, 0.0, 40.0, new PredkoscWiatru())));
+        predkoscWiatru.add(new Etykieta<>("przy słabym wietrze", new FunkcjaTrapezoidalna<>(0.0, 0.0, 3.0, 3.5, POCZATEK_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, KONIEC_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, new PredkoscWiatru())));
+        predkoscWiatru.add(new Etykieta<>("przy umiarkowanym wietrze", new FunkcjaTrapezoidalna<>(3.0, 3.5, 8.0, 9.0, POCZATEK_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, KONIEC_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, new PredkoscWiatru())));
+        predkoscWiatru.add(new Etykieta<>("przy silnym wietrze", new FunkcjaTrapezoidalna<>(8.0, 9.0, 17.0, 20.0, POCZATEK_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, KONIEC_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, new PredkoscWiatru())));
+        predkoscWiatru.add(new Etykieta<>("przy wichrze", new FunkcjaTrapezoidalna<>(17.0, 20.0, 27.0, 30.0, POCZATEK_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, KONIEC_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, new PredkoscWiatru())));
+        predkoscWiatru.add(new Etykieta<>("przy huraganie", new FunkcjaTrapezoidalna<>(27.0, 30.0, 40.0, 40.0, POCZATEK_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, KONIEC_PRZESTRZENI_ROZWAZAN_PREDKOSC_WIATRU, new PredkoscWiatru())));
 
-        iloscOpadow.add(new Etykieta<>("przy niewielkich opadach", new FunkcjaTrapezoidalna<>(0.0, 0.0, 0.1, 0.2, 0.0, 0.5, new IloscOpadow())));
-        iloscOpadow.add(new Etykieta<>("przy umiarowanych opadach", new FunkcjaTrapezoidalna<>(0.1, 0.2, 0.3, 0.35, 0.0, 0.5, new IloscOpadow())));
-        iloscOpadow.add(new Etykieta<>("przy dużych opadach", new FunkcjaTrapezoidalna<>(0.3, 0.35, 0.4, 0.45, 0.0, 0.5, new IloscOpadow())));
-        iloscOpadow.add(new Etykieta<>("przy bardzo dużych opadach", new FunkcjaTrapezoidalna<>(0.4, 0.45, 0.5, 0.5, 0.0, 0.5, new IloscOpadow())));
+        iloscOpadow.add(new Etykieta<>("przy niewielkich opadach", new FunkcjaTrapezoidalna<>(0.0, 0.0, 0.1, 0.2, POCZATEK_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, KONIEC_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, new IloscOpadow())));
+        iloscOpadow.add(new Etykieta<>("przy umiarowanych opadach", new FunkcjaTrapezoidalna<>(0.1, 0.2, 0.3, 0.35, POCZATEK_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, KONIEC_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, new IloscOpadow())));
+        iloscOpadow.add(new Etykieta<>("przy dużych opadach", new FunkcjaTrapezoidalna<>(0.3, 0.35, 0.4, 0.45, POCZATEK_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, KONIEC_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, new IloscOpadow())));
+        iloscOpadow.add(new Etykieta<>("przy bardzo dużych opadach", new FunkcjaTrapezoidalna<>(0.4, 0.45, 0.5, 0.5, POCZATEK_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, KONIEC_PRZESTRZENI_ROZWAZAN_ILOSC_OPADOW, new IloscOpadow())));
 
-        widocznosc.add(new Etykieta<>("przy słaba widoczność", new FunkcjaTrapezoidalna<>(0.0, 0.0, 0.1, 0.3, 0.0, 10.0, new Widocznosc())));
-        widocznosc.add(new Etykieta<>("przy ograniczonej widoczności", new FunkcjaTrapezoidalna<>(0.1, 0.3, 0.7, 1.0, 0.0, 10.0, new Widocznosc())));
-        widocznosc.add(new Etykieta<>("przy dobrej widoczności", new FunkcjaTrapezoidalna<>(0.7, 1.0, 2.0, 3.0, 0.0, 10.0, new Widocznosc())));
-        widocznosc.add(new Etykieta<>("przy pełnej widoczności", new FunkcjaTrapezoidalna<>(2.0, 3.0, 5.0, 10.0, 0.0, 10.0, new Widocznosc())));
+        widocznosc.add(new Etykieta<>("przy słaba widoczność", new FunkcjaTrapezoidalna<>(0.0, 0.0, 0.1, 0.3, POCZATEK_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, new Widocznosc())));
+        widocznosc.add(new Etykieta<>("przy ograniczonej widoczności", new FunkcjaTrapezoidalna<>(0.1, 0.3, 0.7, 1.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, new Widocznosc())));
+        widocznosc.add(new Etykieta<>("przy dobrej widoczności", new FunkcjaTrapezoidalna<>(0.7, 1.0, 2.0, 3.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, new Widocznosc())));
+        widocznosc.add(new Etykieta<>("przy pełnej widoczności", new FunkcjaTrapezoidalna<>(2.0, 3.0, 5.0, 10.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WIDOCZNOSC, new Widocznosc())));
 
-        cisnienie.add(new Etykieta<>("przy bardzo niskim ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 27.0, 27.0, 32.0, new Cisnienie())));
-        cisnienie.add(new Etykieta<>("przy niskim ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 27.75, 27.0, 32.0, new Cisnienie())));
-        cisnienie.add(new Etykieta<>("przy umiarkowanym ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 28.75, 27.0, 32.0, new Cisnienie())));
-        cisnienie.add(new Etykieta<>("przy wysokim ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 29.75, 27.0, 32.0, new Cisnienie())));
-        cisnienie.add(new Etykieta<>("przy bardzo wysokim ciśnieniu", new FunkcjaTrapezoidalna<>(30.0, 30.5, 32.0, 32.0, 27.0, 32.0, new Cisnienie())));
+        cisnienie.add(new Etykieta<>("przy bardzo niskim ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 27.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CISNIENIE, KONIEC_PRZESTRZENI_ROZWAZAN_CISNIENIE, new Cisnienie())));
+        cisnienie.add(new Etykieta<>("przy niskim ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 27.75, POCZATEK_PRZESTRZENI_ROZWAZAN_CISNIENIE, KONIEC_PRZESTRZENI_ROZWAZAN_CISNIENIE, new Cisnienie())));
+        cisnienie.add(new Etykieta<>("przy umiarkowanym ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 28.75, POCZATEK_PRZESTRZENI_ROZWAZAN_CISNIENIE, KONIEC_PRZESTRZENI_ROZWAZAN_CISNIENIE, new Cisnienie())));
+        cisnienie.add(new Etykieta<>("przy wysokim ciśnieniu", new FunkcjaGausowska<>(0.3, 1.0, 29.75, POCZATEK_PRZESTRZENI_ROZWAZAN_CISNIENIE, KONIEC_PRZESTRZENI_ROZWAZAN_CISNIENIE, new Cisnienie())));
+        cisnienie.add(new Etykieta<>("przy bardzo wysokim ciśnieniu", new FunkcjaTrapezoidalna<>(30.0, 30.5, 32.0, 32.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CISNIENIE, KONIEC_PRZESTRZENI_ROZWAZAN_CISNIENIE, new Cisnienie())));
 
-        wilgotnosc.add(new Etykieta<>("przy bardzo suchym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 4.0, 27.0, 32.0, new Wilgotnosc())));
-        wilgotnosc.add(new Etykieta<>("przy suchym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 25.0, 27.0, 32.0, new Wilgotnosc())));
-        wilgotnosc.add(new Etykieta<>("przy umiarkowanie wilgotnym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 50.0, 27.0, 32.0, new Wilgotnosc())));
-        wilgotnosc.add(new Etykieta<>("przy wilgotnym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 75.0, 27.0, 32.0, new Wilgotnosc())));
-        wilgotnosc.add(new Etykieta<>("przy wilgotnym powietrzu", new FunkcjaTrapezoidalna<>(80.0, 90.0, 100.0, 100.0, 27.0, 32.0, new Wilgotnosc())));
+        wilgotnosc.add(new Etykieta<>("przy bardzo suchym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 4.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, new Wilgotnosc())));
+        wilgotnosc.add(new Etykieta<>("przy suchym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 25.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, new Wilgotnosc())));
+        wilgotnosc.add(new Etykieta<>("przy umiarkowanie wilgotnym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 50.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, new Wilgotnosc())));
+        wilgotnosc.add(new Etykieta<>("przy wilgotnym powietrzu", new FunkcjaGausowska<>(8.0, 1.0, 75.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, new Wilgotnosc())));
+        wilgotnosc.add(new Etykieta<>("przy wilgotnym powietrzu", new FunkcjaTrapezoidalna<>(80.0, 90.0, 100.0, 100.0, POCZATEK_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, KONIEC_PRZESTRZENI_ROZWAZAN_WILGOTNOSC, new Wilgotnosc())));
 
-        temperatura.add(new Etykieta<>("przy bardzo zimnej temperaturze", new FunkcjaTrapezoidalna<>(-16.0, -16.0, 14.0, 23.0, -16.0, 104.0, new Temperatura())));
-        temperatura.add(new Etykieta<>("przy zimnej temperaturze", new FunkcjaTrapezoidalna<>(14.0, 23.0, 44.0, 54.0, -16.0, 104.0, new Temperatura())));
-        temperatura.add(new Etykieta<>("przy umiarkowanej temperaturze", new FunkcjaTrapezoidalna<>(44.0, 54.0, 63.0, 71.0, -16.0, 104.0, new Temperatura())));
-        temperatura.add(new Etykieta<>("przy ciepłej temperaturze", new FunkcjaTrapezoidalna<>(63.0, 71.0, 80.0, 90.0, -16.0, 104.0, new Temperatura())));
-        temperatura.add(new Etykieta<>("przy bardzo ciepłej temperaturze", new FunkcjaTrapezoidalna<>(80.0, 90.0, 104.0, 104.0, -16.0, 104.0, new Temperatura())));
+        temperatura.add(new Etykieta<>("przy bardzo zimnej temperaturze", new FunkcjaTrapezoidalna<>(-16.0, -16.0, 14.0, 23.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new Temperatura())));
+        temperatura.add(new Etykieta<>("przy zimnej temperaturze", new FunkcjaTrapezoidalna<>(14.0, 23.0, 44.0, 54.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new Temperatura())));
+        temperatura.add(new Etykieta<>("przy umiarkowanej temperaturze", new FunkcjaTrapezoidalna<>(44.0, 54.0, 63.0, 71.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new Temperatura())));
+        temperatura.add(new Etykieta<>("przy ciepłej temperaturze", new FunkcjaTrapezoidalna<>(63.0, 71.0, 80.0, 90.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new Temperatura())));
+        temperatura.add(new Etykieta<>("przy bardzo ciepłej temperaturze", new FunkcjaTrapezoidalna<>(80.0, 90.0, 104.0, 104.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new Temperatura())));
 
-        temperaturaOdczuwalna.add(new Etykieta<>("przy bardzo zimnej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(-16.0, -16.0, 14.0, 23.0, -16.0, 104.0, new TemperaturaOdczuwalna())));
-        temperaturaOdczuwalna.add(new Etykieta<>("przy zimnej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(14.0, 23.0, 44.0, 54.0, -16.0, 104.0, new TemperaturaOdczuwalna())));
-        temperaturaOdczuwalna.add(new Etykieta<>("przy umiarkowanej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(44.0, 54.0, 63.0, 71.0, -16.0, 104.0, new TemperaturaOdczuwalna())));
-        temperaturaOdczuwalna.add(new Etykieta<>("przy ciepłej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(63.0, 71.0, 80.0, 90.0, -16.0, 104.0, new TemperaturaOdczuwalna())));
-        temperaturaOdczuwalna.add(new Etykieta<>("przy bardzo ciepłej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(80.0, 90.0, 104.0, 104.0, -16.0, 104.0, new TemperaturaOdczuwalna())));
-
-
-        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości poniżej pół mili", new FunkcjaTrojkatna<>(0.0, 0.0, 0.5, 0.0, 6.0, new Odleglosc())));
-        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości około jednej mili", new FunkcjaTrojkatna<>(0.0, 1.0, 2.0, 0.0, 6.0, new Odleglosc())));
-        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości około trzech mili", new FunkcjaTrojkatna<>(1.0, 3.0, 5.0, 0.0, 6.0, new Odleglosc())));
-        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości ponad trzech mili", new FunkcjaTrapezoidalna<>(3.0, 5.0, 6.0, 6.0, 0.0, 6.0, new Odleglosc())));
+        temperaturaOdczuwalna.add(new Etykieta<>("przy bardzo zimnej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(-16.0, -16.0, 14.0, 23.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new TemperaturaOdczuwalna())));
+        temperaturaOdczuwalna.add(new Etykieta<>("przy zimnej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(14.0, 23.0, 44.0, 54.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new TemperaturaOdczuwalna())));
+        temperaturaOdczuwalna.add(new Etykieta<>("przy umiarkowanej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(44.0, 54.0, 63.0, 71.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new TemperaturaOdczuwalna())));
+        temperaturaOdczuwalna.add(new Etykieta<>("przy ciepłej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(63.0, 71.0, 80.0, 90.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new TemperaturaOdczuwalna())));
+        temperaturaOdczuwalna.add(new Etykieta<>("przy bardzo ciepłej temperaturze odczuwalnej", new FunkcjaTrapezoidalna<>(80.0, 90.0, 104.0, 104.0, POCZATEK_PRZESTRZENI_ROZWAZAN_TEMPERATURA, KONIEC_PRZESTRZENI_ROZWAZAN_TEMPERATURA, new TemperaturaOdczuwalna())));
 
 
-        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające poniżej godziny", new FunkcjaTrojkatna<>(0.0, 0.0, 1.0, 0.0, 24.0, new CzasTrwania())));
-        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające około dwóch godzin", new FunkcjaTrojkatna<>(0.0, 2.0, 4.0, 0.0, 24.0, new CzasTrwania())));
-        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające około czterech godzin", new FunkcjaTrojkatna<>(2.0, 4.0, 6.0, 0.0, 24.0, new CzasTrwania())));
-        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające około sześciu godzin", new FunkcjaTrojkatna<>(4.0, 6.0, 8.0, 0.0, 24.0, new CzasTrwania())));
-        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające ponad sześć godzin", new FunkcjaTrojkatna<>(6.0, 8.0, 24.0, 0.0, 24.0, new CzasTrwania())));
+        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości poniżej pół mili", new FunkcjaTrojkatna<>(0.0, 0.0, 0.5, POCZATEK_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, KONIEC_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, new Odleglosc())));
+        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości około jednej mili", new FunkcjaTrojkatna<>(0.0, 1.0, 2.0, POCZATEK_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, KONIEC_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, new Odleglosc())));
+        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości około trzech mili", new FunkcjaTrojkatna<>(1.0, 3.0, 5.0, POCZATEK_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, KONIEC_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, new Odleglosc())));
+        odleglosc.add(new Etykieta<>("wywołuje utrudnienia w ruchu o długości ponad trzech mili", new FunkcjaTrapezoidalna<>(3.0, 5.0, 6.0, 6.0, POCZATEK_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, KONIEC_PRZESTRZENI_ROZWAZAN_ODLEGLOSC, new Odleglosc())));
+
+
+        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające poniżej godziny", new FunkcjaTrojkatna<>(0.0, 0.0, 1.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, KONIEC_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, new CzasTrwania())));
+        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające około dwóch godzin", new FunkcjaTrojkatna<>(0.0, 2.0, 4.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, KONIEC_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, new CzasTrwania())));
+        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające około czterech godzin", new FunkcjaTrojkatna<>(2.0, 4.0, 6.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, KONIEC_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, new CzasTrwania())));
+        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające około sześciu godzin", new FunkcjaTrojkatna<>(4.0, 6.0, 8.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, KONIEC_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, new CzasTrwania())));
+        czasTrwania.add(new Etykieta<>("wywołuje utrudnienia w ruchu trwające ponad sześć godzin", new FunkcjaTrojkatna<>(6.0, 8.0, 24.0, POCZATEK_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, KONIEC_PRZESTRZENI_ROZWAZAN_CZAS_TRWANIA, new CzasTrwania())));
         didInit = true;
     }
 
